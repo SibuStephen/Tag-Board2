8672fa1c2acc0731daa00340bec9b26169280a07
sibstephen <sibustephen591@gmail.com>
2015-04-02 15:31:50 +0530

    Alignment and angular controller

--- controller/Angular-controller.js
+++ controller/Angular-controller.js
@@ -1,72 +1,81 @@
   'use strict';
 
-  var tagboardController = angular.module('tag-board', []);
+  var tagboardController = angular.module('tag-board', [])
 
-  tagboardController.controller('tagboardCtrl', ['$http','$scope','$interval',
-
-    function ($http, $scope,$interval) {
-
+  .controller('tagboardCtrl', ['$http','$scope',
+    function ($http, $scope) {
       $http.get('controller/tabledata.json').success(function(data) {
-
-
         $scope.tag_list = data;
+      });
+  }])
 
-    //     for (var i =0; i < $scope.tag_list.length-1; i++){
+  .directive('tagUpdate', ['$interval', function($interval) {
+    function updateTile(tiles) {
+      tiles.each(function() {
 
-    //      var updated_value = Math.floor(Math.random()*100);
-    //      var this_value = $scope.tag_list[i];
+        var $this = $(this);
+        var $value = $this.find('.detail_tag_name');
+        var $icon = $this.find('.fa');
+        var $updated_value = Math.floor(Math.random()*1000);
+        var $current_value = $value.text();
+        // Update value.
+        $value.text($updated_value);
+        if ($current_value > $updated_value) {
+          $value.css('color', 'green');
+          if ($icon.hasClass('fa-sort-asc')) {
+            $icon.removeClass('fa-sort-asc');
+          }
+          $icon.addClass('fa-sort-desc');
+        }
+        else if ($current_value < $updated_value) {
+          $value.css('color', 'red');
+          if ($icon.hasClass('fa-sort-desc')) {
+            $icon.removeClass('fa-sort-desc');
+          }
+          $icon.addClass('fa-sort-asc');
+        }
+      });
+    }
+    return {
+      link: $interval(function() {
+        var tiles = $('.tile');
+        updateTile(tiles);
+       }, 3000)
+    }
+  }])
 
+  .directive('MyController', ['$scope', function($scope) {
 
-    //      if (this_value.tag_name < updated_value){
+    $scope.show=function()
+  {
 
-    //       this_value.classed = 'green';
-    //       this_value.class_icon = 'fa-sort-asc';
-    //       this_value.tag_name = updated_value;
+  var button1 =document.getElementById("modal_search_hidden");
+  var button2 =document.getElementById("modal_search_hidden");
 
-    //     }
+  button1.style.display="block";
+  button2.style.opacity=1;
 
-    //     else if (this_value.tag_name > updated_value){
+ };
 
-    //       this_value.classed = "red";
-    //       this_value.class_icon = 'fa-sort-desc';
-    //       this_value.tag_name = updated_value;
+}])
 
-    //     }
-    //   }
+// .directive('shows',['$show',function($show){
 
-    //   console.log(this_value);
-    //   console.log(updated_value);
+// function show(button1,button2,scope)
+// {
+//   scope.button1.style.display="block";
+//   scope.button2.style.opacity=1;
+// }
+// return{
+//   link: $show(function()
+//   {
+//     var button1 =document.getElementById("modal_search_hidden");
+//     var button2 =document.getElementById("modal_search_hidden");
+//     scope: {
+//             eventHandler: '&ngClick'
+//            }
+//    show(button1,button2,scope);
+//   })
+// }
+// }])
 
-    //    $scope.call = function(){
-
-    //         this_value.tag_name;
-    // }
-
-    // $interval( function(){$scope.call();}, 500);
-
-    });
-
-
-    }]);
-
-
-  <!-- -->
-
-    // for (var i = $scope.tag_list.length - 1; i >= 0; i--) {
-    //   var $this = $scope.tag_list[i];
-
-    //   if ($this.tag_name < $this.upper_value) {
-    //     $this.classed = 'green';
-    //     $this.class_icon = 'fa-sort-asc';
-    //   }
-    //   else if ($this.tag_name > $this.upper_value) {
-
-    //     $this.classed="red";
-    //     $this.class_icon = 'fa-sort-desc';
-    //   }
-
-    //   else {
-    //     $this.classed="black";
-
-    //   }
-    // }
